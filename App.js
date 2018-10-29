4ab15a4f4f27c21b640bbe76807d4c79bd904ad1
Kintzmor000 <Kintzmor000@edmonds15.org>
2018-10-29 11:05:59 -0700

    :egg: Upload first Build

---
+++ App.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { TouchableWithoutFeedback, StyleSheet, Button, Alert, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  _onPressButton() {
+    Alert.alert('You Better Be!')
+  }
+  
+  render() {
+    return (
+
+      <View style={styles.container}>
+          <Text style={styles.textMain}>Mushroom Identifier</Text>
+
+        <View style={styles.buttonContainer}>
+          <TouchableWithoutFeedback
+            onPress={this._onPressButton}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Ready?</Text>
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={this._onPressButton}>
+            <View style={styles.mushroomStalk}>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+
+
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FEFACB',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginTop: 20,
+    alignSelf: 'center',
+    width: 100,
+    alignItems: 'center',
+    backgroundColor: "#CC1516",
+    borderRadius: 10,
+  },
+  buttonText: {
+    padding: 10,
+    color: "#FEFACB",
+  },
+  mushroomStalk: {
+    backgroundColor: "#EBD489",
+    width: 25,
+    height: 50,
+    alignSelf: 'center',
+    borderBottomEndRadius: 10,
+  },
+  textMain: {
+    fontSize: 32,
+    color: "#CC1516",
+  }
+});
